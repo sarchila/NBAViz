3a0c8d5d443ceb548d8b90b331b26efb100c2dfc
Santiago <santiago.archila@gmail.com>
2014-02-05 20:33:01 -0800

    set up remote heroku mongoDB

--- config/env/development.js
+++ config/env/development.js
@@ -1,26 +1,4 @@
 module.exports = {
-    db: "mongodb://localhost/mean-dev",
-    app: {
-        name: "NBA Stats"
-    },
-    facebook: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
-    },
-    twitter: {
-        clientID: "CONSUMER_KEY",
-        clientSecret: "CONSUMER_SECRET",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    github: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/github/callback"
-    },
-    google: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/google/callback"
-    }
-}
+  db: "mongodb://localhost:27017/nba-viz",
+  app: {name: "NBA Viz"}
+};
--- config/env/test.js
+++ config/env/test.js
@@ -1,27 +1,7 @@
 module.exports = {
-    db: "mongodb://localhost/mean-test",
+    db: "mongodb://localhost/nba-viz-test",
     port: 3001,
     app: {
-        name: "MEAN - A Modern Stack - Test"
-    },
-    facebook: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
-    },
-    twitter: {
-        clientID: "CONSUMER_KEY",
-        clientSecret: "CONSUMER_SECRET",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    github: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/github/callback"
-    },
-    google: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        name: "NBA Viz - Test"
     }
 }
